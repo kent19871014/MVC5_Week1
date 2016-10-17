742349b286849b5cf961fbf242830b75e7af2ca7
KentWu <kent19871014@gmail.com>
2016-10-17 22:13:21 +0800

    加入專案檔案。

---
+++ MVC5_Week1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_Week1.MvcApplication" Language="C#" %>
---
+++ MVC5_Week1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_Week1.Startup))]
+namespace MVC5_Week1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
